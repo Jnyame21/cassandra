7f01ad35ee1df639aef1e3d447abb4d8a2885000
Justice Nyame <94219365+Jnyame21@users.noreply.github.com>
2024-04-21 11:31:06 +0000

    minor bug fixes

--- Backend/Backend/production.py
+++ Backend/Backend/production.py
@@ -1,5 +1,6 @@
 from Backend.settings import *
 from google.oauth2 import service_account
+import json
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -14,7 +15,14 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 # Media Config (Google Cloud Storage)
-GS_CREDENTIALS = service_account.Credentials.from_service_account_info(os.environ.get("GS_CREDENTIALS"))
+credentials_info = os.environ.get('GCS_CREDENTIALS')
+if credentials_info:
+    credentials_dict = json.loads(credentials_info)
+    GS_CREDENTIALS = service_account.Credentials.from_service_account_info(credentials_dict)
+    
+else:
+    raise ValueError("GCS_CREDENTIALS environment variable is not set")
+
 
 GS_MEDIA_URL = 'https://storage.googleapis.com/eduaap-bkt/media/'
 
